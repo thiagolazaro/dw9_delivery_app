2a5e2de1a3f53f8fe50afb6fa0be0a25154d27a1
Thiago Lázaro <thiagolazaro2009@gmail.com>
2023-01-29 22:33:55 -0300

    Start app

---
+++ lib/dw9_delivery_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dw9DeliveryApp extends StatelessWidget {
+  const Dw9DeliveryApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery App',
+      routes: {
+        '/': (context) => Container(),
+      },
+    );
+  }
+}
