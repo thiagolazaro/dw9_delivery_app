5deaf42b64e9d27f11491fa18fe2e27796887c7f
Thiago Lázaro <thiagolazaro2009@gmail.com>
2023-01-30 22:22:41 -0300

    Criado e configurado o tema

--- lib/app/dw9_delivery_app.dart
+++ lib/app/dw9_delivery_app.dart
@@ -1,6 +1,8 @@
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
+import 'core/ui/theme/theme_config.dart';
+
 class Dw9DeliveryApp extends StatelessWidget {
   const Dw9DeliveryApp({Key? key}) : super(key: key);
 
@@ -8,6 +10,7 @@ class Dw9DeliveryApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Delivery App',
+      theme: ThemeConfig.theme,
       routes: {
         '/': (context) => const SplashPage(),
       },
--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,4 +1,3 @@
-import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -6,35 +5,18 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          color: Colors.white,
-          elevation: 0,
-          centerTitle: true,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-        primaryColor: ColorsApp.i.primary,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: ColorsApp.i.primary,
-          primary: ColorsApp.i.primary,
-          secondary: ColorsApp.i.secondary,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
       ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Splash'),
-        ),
-        body: Column(
-          children: [
-            Container(),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Teste'),
-            )
-          ],
-        ),
+      body: Column(
+        children: [
+          Container(),
+          ElevatedButton(onPressed: () {}, child: const Text('Teste')),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'text'),
+          )
+        ],
       ),
     );
   }
